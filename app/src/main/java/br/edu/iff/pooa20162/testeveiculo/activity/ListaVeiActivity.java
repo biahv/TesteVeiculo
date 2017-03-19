3eb0e25545003aa2323515a1cdce443f7970e053
biahv <beatriz.hviana@gmail.com>
2017-03-19 20:57:54 -0300

    Alterando Veículos

--- app/src/main/java/br/edu/iff/pooa20162/testeveiculo/activity/ListaVeiActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/testeveiculo/activity/ListaVeiActivity.java
@@ -47,7 +47,7 @@ public class ListaVeiActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lista_propri, menu);
+        getMenuInflater().inflate(R.menu.menu_lista_vei, menu);
         return true;
     }
 
